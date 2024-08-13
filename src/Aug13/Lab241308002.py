b8947f8eb0911fb75aeb2efcee4100737bc9593a
bachk001 <kalyanibachhav3@gmail.com>
2024-08-13 09:04:19 +0530

    Commit

---
+++ src/Aug13/Lab241308002.py
@@ -0,0 +1,24 @@
+"""
+create a Program I will give you number(userinput and print table)
+f"{}" String format concept
+User input - num int -> 10, 100, -1, 2, 3.14 -> input
+9x1 = 9
+9x2 = 18... till 10
+"""
+
+num = int(input("Enter the number : "))
+print("Table of ",num)
+print(f"{num}*1={num}")
+print(f"{num}*2={num}")
+print(f"{num}*3={num}")
+print(f"{num}*4={num}")
+print(f"{num}*5={num}")
+print(f"{num}*6={num}")
+print(f"{num}*7={num}")
+print(f"{num}*8={num}")
+print(f"{num}*9={num}")
+print(f"{num}*10={num}")
+
+
+
+
