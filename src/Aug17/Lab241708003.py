c9fa139f4c65fb166500d0d7171203c72d8519b5
bachk001 <kalyanibachhav3@gmail.com>
2024-08-21 23:12:54 +0530

    Commit

---
+++ src/Aug17/Lab241708003.py
@@ -0,0 +1,10 @@
+### Task #3
+
+
+
+- Explain the difference between the = operator and the == operator in Python.
+
+- What does the ** operator do in Python, and how is it used?
+
+- What does the ^ operator do in Python, and in what context is it commonly used?
+
