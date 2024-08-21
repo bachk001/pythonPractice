485b0155bb98b80b29e3819c4bc1f4c174605409
bachk001 <kalyanibachhav3@gmail.com>
2024-08-21 23:17:21 +0530

    Commit

--- src/Aug17/Lab241708003.py
+++ src/Aug17/Lab241708003.py
@@ -1,10 +1,13 @@
+"""
 ### Task #3
-
-
-
 - Explain the difference between the = operator and the == operator in Python.
-
 - What does the ** operator do in Python, and how is it used?
-
 - What does the ^ operator do in Python, and in what context is it commonly used?
+""""""
 
+#  = is used for assignment
+#  == is used for comparision
+
+#  **  is used for exponentiation
+
+# ^ bitwise XOR operation
