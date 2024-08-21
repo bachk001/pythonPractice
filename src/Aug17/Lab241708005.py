09402f8ab73cd15bd75c706bf630c1750f03e74d
bachk001 <kalyanibachhav3@gmail.com>
2024-08-21 23:41:55 +0530

    Commit

--- src/Aug17/Lab241708005.py
+++ src/Aug17/Lab241708005.py
@@ -1,5 +1,15 @@
-### Task #5
+"""
+Task #5
+Create a program that takes two numbers as input and
+prints whether the first number is greater than, less than, or equal to the second number.
+"""
 
+num1 = float(input("Enter num1 : "))
+num2 = float(input("Enter num2 : "))
 
-
-- Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+if num1>num2:
+    print("Number ", num1, " is greater than ", num2)
+elif num1==num2:
+    print("Number ", num1, " is equal to ", num2)
+else:
+    print("Number ", num1, " is smaller than ", num2)
--- src/Aug17/Lab241708006.py
+++ src/Aug17/Lab241708006.py
@@ -3,3 +3,7 @@
 
 - Develop a Python script that calculates the square and cube of a given number.
 """
+
+num = float(input("Enter the number : "))
+print("Square of number is ",num**2)
+print("Cube of number is ",num**3)
